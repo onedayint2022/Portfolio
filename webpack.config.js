45cf6e966269200bb2cbea94adf4f589f5747b2a
onedayint2022 <onedayint2017@gmail.com>
2024-04-11 22:31:46 +1000

    :bug: test for build

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.tsx',
 
   output: {
-    path: path.join(__dirname, '/dist'),
+    path: path.join(__dirname, '/build'),
     filename: 'bundle.js',
     publicPath: '/',
   },
